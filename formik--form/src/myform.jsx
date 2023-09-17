73c28969b94146e76e319ac598036f745eca1ebf
Natissa-DidyToure <natissadidy@gmail.com>
2023-09-17 13:32:04 -0400

    installation de lappli + installation de formik et yup
    jai clone lexercice egalement

---
+++ formik--form/src/myform.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const MyForm = () => {
+    return (
+        <Formik
+            initialValues={{ name: '', email: '', password: '' 
+            }}
+            validationSchema={Yup.object({
+                name: Yup.string()
+                    .max(15, 'Must be 15 characters or less')
+                    .required('Required'),
+                email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Required'),
+                password: Yup.string()
+                    .min(8, 'Must be 8 characters or more')
+                    .required('Required'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 1000);
+            }}
+        >
+            <Form>
+                <div>
+                <label htmlFor="name">Name</label>
+                <Field name="name" type="text" />
+                <ErrorMessage name="name" />
+                </div>
+                <div>
+                <label htmlFor="email">Email Address</label>
+                <Field name="email" type="email" />
+                <ErrorMessage name="email" />
+                </div>
+                <div>
+                <label htmlFor="password">Password</label>
+                <Field name="password" type="password" />
+                <ErrorMessage name="password" />
+                </div>
+                <button type="submit">Submit</button>
+            </Form>
+        </Formik>
+
+    );
+};
+
+export default MyForm;
